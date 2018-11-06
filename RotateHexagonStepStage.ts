ade155dd75bab0befe0f646a5eaed6fd2d7cbf7b
Anwesh43 <anweshthecool0@gmail.com>
2018-11-06 13:29:30 +0530

    adding State class

--- RotateHexagonStepStage.ts
+++ RotateHexagonStepStage.ts
@@ -30,3 +30,34 @@ class RotateHexagonStepStage {
         stage.handleTap()
     }
 }
+
+const divideScale : Function = (scale : number, i : number, n : number) : number => Math.min(1/n, Math.max(0, scale - (1/n) * i)) * n
+
+const getSf : Function = (sc : number) : number => Math.floor(sc/0.5)
+
+const scaleFactor : Function = (dir : number, scale : number) : number => {
+    return 0.05 * this.dir * ((1 - getSf())/lines + getSf())
+}
+
+class State {
+    scale : number = 0
+    dir : number = 0
+    prevScale : number = 0
+
+    update(cb : Function) {
+        this.scale += scaleFactor(this.dir, this.scale)
+        if (Math.abs(this.scale - this.prevScale) > 1) {
+            this.scale = this.prevScale + this.dir
+            this.dir = 0
+            this.prevScale = this.scale
+            cb()
+        }
+    }
+
+    startUpdating(cb : Function) {
+        if (this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale
+            cb()
+        }
+    }
+}
