e60a426e4ecf6805143fe75974569eea87c55995
Anwesh43 <anweshthecool0@gmail.com>
2018-11-06 14:03:47 +0530

    adding RotateHexagonStep class

--- RotateHexagonStepStage.ts
+++ RotateHexagonStepStage.ts
@@ -122,6 +122,9 @@ class RHSNode {
             context.restore()
         }
         context.restore()
+        if (this.next) {
+            this.next.draw(context)
+        }
     }
 
     update(cb : Function) {
@@ -142,5 +145,31 @@ class RHSNode {
         }
         cb()
         return this
+    }
+}
+
+class RotateHexagonStep {
+
+    root : RHSNode = new RHSNode(0)
+
+    curr : RHSNode = this.root
+
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.root.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
     }
 }
